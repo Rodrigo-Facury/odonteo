8c50b5bda6ed334764d3fe594e44d1ab0ca746b2
Brayan Wilis <BrayanWilis@outlook.com>
2022-11-24 22:14:23 -0300

    [ Feat ]: Adicionei novos testes

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -8,7 +8,6 @@ describe("Login", () => {
   beforeEach(() => {
     window.history.replaceState(window.history.state, "", "/");
   });
-
   it("Renderizou a página de inicio", () => {
     window.history.replaceState({}, "", "/");
     render(<App />, { wrapper: BrowserRouter });
@@ -16,7 +15,6 @@ describe("Login", () => {
     expect(screen.getByLabelText("Email:")).toBeInTheDocument();
     expect(screen.getByLabelText("Senha:")).toBeInTheDocument();
   });
-
   it("Deu email invalido com a validação", () => {
     window.history.replaceState({}, "", "/");
     render(<App />, { wrapper: BrowserRouter });
@@ -28,7 +26,6 @@ describe("Login", () => {
       screen.getByText("Email ou senha em formato incorreto.")
     ).toBeInTheDocument();
   });
-
   it("Deu senha invalido com a validação", () => {
     window.history.replaceState({}, "", "/");
     render(<App />, { wrapper: BrowserRouter });
@@ -40,7 +37,6 @@ describe("Login", () => {
       screen.getByText("Email ou senha em formato incorreto.")
     ).toBeInTheDocument();
   });
-
   it("Credenciais que não batem", async () => {
     window.history.replaceState({}, "", "/");
     render(<App />, { wrapper: BrowserRouter });
@@ -61,11 +57,10 @@ describe("Login", () => {
 
 describe("Main", () => {
   beforeEach(() => {
-    window.history.replaceState(window.history.state, "", "/statement");
+    window.history.replaceState(window.history.state, "", "/");
     window.localStorage.removeItem("token");
     window.localStorage.removeItem("user");
   });
-
   it("Redirecionou para o login", () => {
     window.history.replaceState({}, "", "/");
     render(<App />, { wrapper: BrowserRouter });
@@ -73,6 +68,7 @@ describe("Main", () => {
     expect(screen.getByLabelText("Senha:")).toBeInTheDocument();
     expect(screen.getByText("Entrar")).toBeInTheDocument();
   });
+
   it("Não redireciona contanto que esteja com token", () => {
     window.history.replaceState({}, "", "/");
     window.localStorage.setItem(
@@ -85,6 +81,20 @@ describe("Main", () => {
     expect(screen.getByLabelText("Email:")).not.toBeInTheDocument();
     expect(screen.getByLabelText("Senha:")).not.toBeInTheDocument();
     expect(screen.getByText("Entrar")).not.toBeInTheDocument();
+  });
+  it("Redireciona para a consulta", () => {
+    window.history.replaceState({}, "", "/");
+    window.localStorage.setItem(
+      "token",
+      JSON.stringify(
+        "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2OTU5NjU5MCwiaWF0IjoxNjY5MzM3MzkwfQ.j47olYr5mUair_W4QlQt1CkrTUP52Tjp9y-mLzTy4cc"
+      )
+    );
+    render(<App />, { wrapper: BrowserRouter });
+    fireEvent.click(screen.getByText("Ver extrato"));
+    expect(screen.getByLabelText("Data de início:")).not.toBeInTheDocument();
+    expect(screen.getByLabelText("Data de fim:")).not.toBeInTheDocument();
+    expect(screen.getByText("Consultar faturamento")).not.toBeInTheDocument();
   });
   it("Valida quando tem informação incorreta", async () => {
     window.history.replaceState({}, "", "/");
@@ -151,5 +161,61 @@ describe("Main", () => {
         ).toBeInTheDocument(),
       { timeout: 3000 }
     );
+  });
+});
+
+describe("Statement", () => {
+  beforeEach(() => {
+    window.history.replaceState(window.history.state, "", "/statement");
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("user");
+  });
+
+  it("Volta na Main quando aperta voltar", async () => {
+    window.history.replaceState({}, "", "/statement");
+    window.localStorage.setItem(
+      "token",
+      JSON.stringify(
+        "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2OTU5NjU5MCwiaWF0IjoxNjY5MzM3MzkwfQ.j47olYr5mUair_W4QlQt1CkrTUP52Tjp9y-mLzTy4cc"
+      )
+    );
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: "3534532143",
+      })
+    );
+    render(<App />, { wrapper: BrowserRouter });
+    fireEvent.click(screen.getByText("Voltar"));
+    expect(screen.getByLabelText("Quantia a receber:")).toBeInTheDocument();
+    expect(screen.getByLabelText("Número de parcelas:")).toBeInTheDocument();
+    expect(
+      screen.getByLabelText("Dia do mês para cobrança:")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText("Data da primeira parcela:")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Registrar cobrança")).toBeInTheDocument();
+  });
+
+  it("Aparece a tabela de faturas se clicar em consultar", async () => {
+    window.history.replaceState({}, "", "/statement");
+    window.localStorage.setItem(
+      "token",
+      JSON.stringify(
+        "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2OTU5NjU5MCwiaWF0IjoxNjY5MzM3MzkwfQ.j47olYr5mUair_W4QlQt1CkrTUP52Tjp9y-mLzTy4cc"
+      )
+    );
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        id: "3534532143",
+      })
+    );
+    render(<App />, { wrapper: BrowserRouter });
+    fireEvent.click(screen.getByText("Consultar faturamento"));
+    expect(screen.getByText("x")).toBeInTheDocument();
+    expect(window.document.querySelector(".statement")).toBeInTheDocument();
+    expect(screen.getByRole("table")).toBeInTheDocument();
   });
 });
